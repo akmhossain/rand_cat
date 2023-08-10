b1df62caacd9534a7790933cde5998bb364bd972
RahiminASC23 <ramohammed@stu.allstarcode.org>
2023-08-09 22:10:14 -0400

    forgor

--- script.js
+++ script.js
@@ -1,39 +1,30 @@
 const api_key = 'live_ncL0WbgwNORxUeFtYdYCBDDdXxQX0el6H3e4nVjQl5eQZrYCNqvTdEgSAqPWcwae';
 
-let img_container = document.querySelector('img')
-let cat_caption = document.getElementById('cat_name')
-let change_button = document.getElementById('change')
+let img_container = document.querySelector('img');
+let cat_caption = document.getElementById('cat_name');
+let change_button = document.getElementById('change');
+
+function randInt(max) {
+    return Math.floor(Math.random() * max);
+}
 
 function rand_img() {
-    let url = ' https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key' + api_key;
-    console.log('requst url:', url)
-    img_container.src = 'url'
+    let url = 'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=' + api_key;
+    console.log('request url:', url);
+
     fetch(url)
     .then(function(response) {
         return response.json();
     })
     .then(function(myJson) {
-        let img = myJson.photos[randInt(myJson.photos.length)];
+        let img = myJson[0];
         console.log(img);
 
-        img_container.src = img.img_src;
-        caption.innerHTML = img.camera.full_name + " " + img.earth_date;
+        img_container.src = img.url;
+        cat_caption.innerHTML = "This is " + img.breeds[0].name;
     });
 }
 
-rand_img()
-     
-    fetch(url)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        let img = myJson.photos[randInt(myJson.photos.length)];
-        console.log(img);
-
-        img_container.src = img.img_src;
-        caption.innerHTML = img.camera.full_name + " " + img.earth_date;
-    });
-
-
 rand_img();
+
+change_button.addEventListener('click', rand_img);
